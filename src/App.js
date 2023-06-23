c79d40cff36c9092ef4e3ce56062221802542b66
Daniela Odhiambo <98682114+DanielaOdhis@users.noreply.github.com>
2023-06-23 14:40:25 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherApp from './weather.js';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
+};
+
+export default App;
